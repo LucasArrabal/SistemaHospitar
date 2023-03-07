aba409bb7e3d26c5f20d89562917b7baa14fb947
Lucas Arrabal <larrabal@PRODAM.SP.GOV.BR>
2023-03-07 17:08:35 -0300

    Adicionar arquivos de projeto.

---
+++ Cadastro/Controllers/PacienteController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cadastro.Controllers
+{
+    public class PacienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
