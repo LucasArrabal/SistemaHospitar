f3942fc0cb985063bed1a3a41e01ebe6f1a57746
LucasArrabal <lucas.arrabal2003@gmail.com>
2023-04-26 09:12:11 -0300

    teste

---
+++ Cadastro/Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using Cadastro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cadastro.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<PacienteModel> Paciente { get; set;}
+    }
+}
---
+++ Cadastro/Models/PacienteModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Cadastro.Models
+{
+    public class PacienteModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Diagnostico { get; set; }
+        public DateTime DtNascimento { get; set; }
+        public DateTime DtEntrada { get; set; }
+        public string Status { get; set; } 
+
+    }
+}
