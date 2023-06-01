546ead6323ca49554fb6f69c05fcbfac2965aa74
LucasArrabal <lucas.arrabal2003@gmail.com>
2023-06-01 11:06:39 -0300

    commit_02

--- Cadastro/Controllers/PacienteController.cs
+++ Cadastro/Controllers/PacienteController.cs
@@ -1,24 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cadastro.Models;
+using Cadastro.Repositorio;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cadastro.Controllers
 {
     public class PacienteController : Controller
     {
+        private readonly IPacienteRepositorio _pacienteRepositorio;
+        public PacienteController(IPacienteRepositorio pacienteRepositorio)
+        {
+            _pacienteRepositorio = pacienteRepositorio;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Paciente> pacientes = _pacienteRepositorio.BuscarTodos();
+            return View(pacientes);
         }
         public IActionResult Criar()
         {
             return View();
         }
-        public IActionResult Editar()
+        public IActionResult Editar(int id)
         {
-            return View();
+            Paciente paciente = _pacienteRepositorio.ListarPorId(id);
+            return View(paciente);
         }
-        public IActionResult Apagar()
+        public IActionResult Apagar(int id)
         {
-            return View();
+            Paciente paciente = _pacienteRepositorio.ListarPorId(id);
+            return View(paciente);
         }
+        public IActionResult Deletar(int id)
+        {
+            _pacienteRepositorio.Deletar(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Paciente paciente)
+        {
+            _pacienteRepositorio.Adicionar(paciente);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Alterar(Paciente paciente)
+        {
+            _pacienteRepositorio.Atualizar(paciente);
+            return RedirectToAction("Index");
+        }
+
     }
 }
