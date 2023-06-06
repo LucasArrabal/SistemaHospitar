d98e9c76d01d60c09affd38256c5e6c91df36846
LucasArrabal <lucas.arrabal2003@gmail.com>
2023-06-06 10:18:47 -0300

    commit

--- Cadastro/Models/Paciente.cs
+++ Cadastro/Models/Paciente.cs
@@ -1,12 +1,20 @@
-﻿namespace Cadastro.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Cadastro.Models
 {
     public class Paciente
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Digite seu nome")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Informe o diagnostico")]
         public string Diagnostico { get; set; }
+        [Required(ErrorMessage = "Informe o dia do seu nascimento")]
         public DateTime DtNascimento { get; set; }
+        [Required(ErrorMessage = "Informe a data de entrada")]
         public DateTime DtEntrada { get; set; }
+        [Required(ErrorMessage = "Informe a situação do paciente")]
         public string Status { get; set; } 
 
     }
