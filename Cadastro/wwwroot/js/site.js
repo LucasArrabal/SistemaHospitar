d0f8e12a8381d34c1d3644626133355a19f2290b
LucasArrabal <lucas.arrabal2003@gmail.com>
2023-06-28 10:34:20 -0300

    tttt

--- Cadastro/wwwroot/js/site.js
+++ Cadastro/wwwroot/js/site.js
@@ -2,6 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let table = new DataTable('#table-paciente');
+
+
+
 $('.close-alert').click(function () {
     $('.alert').hide('hdie');
 });
